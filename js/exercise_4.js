165b35b13794dbbf1b82d28cb03ed67e9537fa0f
cwiggans <master.char.w@gmail.com>
2016-04-25 10:48:03 -0500

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -41,7 +41,7 @@ var clickHandler = function(e){
   
   $('info').fadeIn(400, function(){
     
-    var info = '';
+    var info = ' ';
     
     info += '<div>',
     info += 	'<h2>' + feature.properties.LABEL + '</h2>';
