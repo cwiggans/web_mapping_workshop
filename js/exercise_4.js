cd8298909d3513e8bad434db47758c28ce14264b
cwiggans <master.char.w@gmail.com>
2016-04-25 11:14:37 -0500

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -65,3 +65,25 @@ map.on('click', function(){
   $('#info').empty();
   
 })
+
+var myLocation = L.mapbox.featureLayer()
+	myLocation.addTo(map);
+
+map.on('locationfound', function(e){
+  
+  myLocation.setGeoJSON({
+    type: 'Feature',
+    geometry: {
+    	type: 'Point',
+      	coordinates: [e.latlng.lng, e.latlng.lat]
+    },
+    properties: {
+      "title": 'Here I am!',
+      "marker-color": '#ff8888',
+      "marker-symbol": 'star'
+    }
+  })
+  
+})
+
+map.locate({ setView: true });
